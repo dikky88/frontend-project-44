6461bb923f25fe652d048f02800d66f2e3c8b6ce
denis git <testmail@mail.ru>
2022-12-04 16:51:01 +0500

    step 7 brain-gcd

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/f71d934b54d9d9154a7c/maintainability)](https://codeclimate.com/github/dikky88/frontend-project-44/maintainability)
 [![asciicast](https://asciinema.org/a/Gb4F5nmc4FhINMwqB05eApaPS.svg)](https://asciinema.org/a/Gb4F5nmc4FhINMwqB05eApaPS)
 [![asciicast](https://asciinema.org/a/23h2jkYh0KioileUSaIEP3wBf.svg)](https://asciinema.org/a/23h2jkYh0KioileUSaIEP3wBf)
+[![asciicast](https://asciinema.org/a/iBdU4jTxOHsAH52RxM7g44m6a.svg)](https://asciinema.org/a/iBdU4jTxOHsAH52RxM7g44m6a)
---
+++ src/games/game-gcd.js
@@ -0,0 +1,36 @@
+import gameHello from '../index.js';
+
+const gameMessage = 'Find the greatest common divisor of given numbers.';
+
+const gameValues = () => {
+  const result = [];
+  for (let i = 0; i < 3; i += 1) {
+    const arr = [];
+    const values = [];
+    const randNumber1 = Math.floor(Math.random() * 100);
+    const randNumber2 = Math.floor(Math.random() * 100);
+    values.push(`Question: ${randNumber1} ${randNumber2}`);
+    arr.push(values);
+    const minNumber = Math.min(randNumber1, randNumber2);
+    const maxNumber = Math.max(randNumber1, randNumber2);
+    let nod = 1;
+    if (maxNumber % minNumber !== 0) {
+      for (let j = 2; j <= minNumber / 2; j += 1) {
+        if (maxNumber % j === 0 && minNumber % j === 0) {
+          nod = j;
+        }
+      }
+    } else if (maxNumber % minNumber === 0) {
+      nod = minNumber;
+    }
+    arr.push(nod);
+    result.push(arr);
+  }
+  return result;
+};
+
+const gameStart = () => {
+  gameHello(gameMessage, gameValues());
+};
+
+export default gameStart;
