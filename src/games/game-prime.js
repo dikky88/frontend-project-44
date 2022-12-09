a85b8aaf248d2afac1b19531a6b562503ec34327
denis git <testmail@mail.ru>
2022-12-09 17:11:07 +0500

    codeclimate refactoring game-prime

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -7,15 +7,13 @@ const gameValues = () => {
   for (let i = 0; i < 3; i += 1) {
     let answer = 'yes';
     const randNumber = Math.floor(Math.random() * 30) + 2;
-    const arr = [];
     for (let j = 2; j < randNumber; j += 1) {
       if (randNumber % j === 0) {
         answer = 'no';
         break;
       }
     }
-    arr.push([`Question: ${randNumber}`], answer);
-    result.push(arr);
+    result.push([[`Question: ${randNumber}`], answer]);
   }
   return result;
 };
